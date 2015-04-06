68248b62c64481efbf14ba910b43e663e7d7a0c2
PinmanHuang <102503512@cc.ncu.edu.tw>
2015-04-06 18:03:21 +0800

    Modify array existed value.

--- DataStructure/Homework1Ref/main.c
+++ DataStructure/Homework1Ref/main.c
@@ -32,17 +32,21 @@ int main(int argc, char **argv) {
 	for(index = optind; index < argc; index++) {
 		printf("Non-option argument %s \n",argv[index]);
 	}
+
 	FILE *fptrR = fopen(infName, "r"), *fptrW = fopen(outfName, "w");
 	if(fptrR == NULL)
 		printf("Error while opening the file. \n");
 	while((in[counter] = fgetc(fptrR)) != EOF) {
 		counter++;
+		printf("counter: %d %c \n",counter, in[counter-1]);
 	}
 	fclose(fptrR);
-	hex_decode(in, strlen(in) ,out);
+	printf("inLen: %d \n", strlen(in)); 
+	hex_decode(in, (strlen(in)-2) ,out);
 	for(int i = 0; i < (counter/2); i++) {
 		printf("%d: %c \n", i, out[i]);
 	}
+	printf("outLen: %d \n", strlen(out));
 	enc = base64_encode(out, strlen(out));
 	printf("orginal: %s \n", out);
 	printf("base64 encoded: %s \n", enc);
