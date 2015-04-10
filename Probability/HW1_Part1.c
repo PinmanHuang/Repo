a5a29e9eb39bd52f432e14f465625d2274e51926
Pinman <pinmanhuang@gmail.com>
2015-04-10 20:08:45 +0800

    update HW1_Part1

--- Probability/HW1_Part1.c
+++ Probability/HW1_Part1.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-	int blackCard = 80, redCard = 80, totalCard, counter = 0;
-	char color;
+	int blackCard = 80, redCard = 80, counter = 0, color;
 	float probability;
-	while(scanf("%c", &color) && color != '0') {
-		if(blackCard == 0 && color == 'B' && totalCard != 0) {
-			printf("There is no Black Card now. \n");
-			printf("You just can enter R. \n");
-			continue;
+	printf("1: 黑色牌\t 2: 紅色牌\n");
+	printf("請輸入1或2表示抽到的牌型，輸入-1結束程式\n");
+	while(scanf("%d", &color) && color != -1) {
+        if(color == -1)
+            break;
+        else if(color == 1) {
+            if(blackCard != 0)
+                blackCard--;
+            else {
+                printf("目前已經沒有黑牌可以抽出 \n");
+                printf("只能輸入2表示目前抽出紅牌 \n");
+                continue;
+            }
 		}
-		if(redCard == 0 && color == 'R' && totalCard != 0) {
-			printf("There is no Red Card now. \n");
-			printf("You just can enter B. \n");
-			continue;
+		else if(color == 2) {
+            if(redCard != 0)
+                redCard--;
+            else {
+                printf("目前已經沒有紅牌可以抽出 \n");
+                printf("只能輸入1表示目前抽出黑牌 \n");
+                continue;
+            }
 		}
-		if(color == 'B' && blackCard != 0) {
-			blackCard--;
+		else {
+            printf("Error: 輸入牌型錯誤，請輸入1或2表示抽到的牌型，輸入-1結束程式 \n");
+            continue;
 		}
-		if(color == 'R' && redCard != 0) {
-			redCard--;
-		}
-		totalCard = redCard + blackCard;
-		if(totalCard == 0) {
+		if(blackCard == 0 && redCard == 0) {
 			blackCard = 80;
 			redCard = 80;
-			totalCard = redCard + blackCard;
 		}
-		probability = (float)blackCard/(float)totalCard;
-		printf("%d / %d = %f\n", blackCard, totalCard, probability);
-		printf("%d\n", counter);
-		counter++;
+		printf("下一次抽中牌滿足洛神效果機率: %d/%d = %f \n", blackCard, blackCard+redCard, (float)blackCard/(blackCard+redCard));
 	}
 	return 0;
 }
