513a72c0f4b583907ebb28e7858810c8fa0f89c7
PinmanHuang <102503512@cc.ncu.edu.tw>
2015-04-10 19:58:53 +0800

    add something

--- Probability/HW1_Part2.c
+++ Probability/HW1_Part2.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 int main() {
+	float probability = 1.0;
+	int numCard, i;
+	printf("Please enter the number of card: ");
+	scanf("%d", &numCard);
+	for(i = 0; i < numCard; i++) {
+		probability = probability * (80 - i)/(160 - i);
+	}
+	printf("Probability: %.14f \n", probability);
 	return 0;
 }
