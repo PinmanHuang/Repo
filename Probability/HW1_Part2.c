02cb875d2b1bccecfcd4dfc73bfac16eee5038c1
Pinman <pinmanhuang@gmail.com>
2015-04-10 20:11:51 +0800

    probability homework one --version2.ch

--- Probability/HW1_Part2.c
+++ Probability/HW1_Part2.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-int main() {
-	float probability = 1.0;
-	int numCard, i;
-	printf("Please enter the number of card: ");
-	scanf("%d", &numCard);
-	for(i = 0; i < numCard; i++) {
-		probability = probability * (80 - i)/(160 - i);
-	}
-	printf("Probability: %.14f \n", probability);
-	return 0;
+#include <stdlib.h>
+
+int main()
+{
+    float probability = 1.0;
+    int numCard, i;
+    printf("請輸入連續抽中洛神效果張數: ");
+    scanf("%d", &numCard);
+    for(i = 0; i < numCard; i++)
+        probability = probability*(80 - i)/(160 - i);
+    if(numCard <= 80)
+        printf("連續抽中%d張為洛神效果的機率為: %f \n", numCard, probability);
+    else
+        printf("輸入張數: %d 大於黑色牌張數: 80 \n", numCard);
+    return 0;
 }
--- Probability/HW1_Part3.c
+++ Probability/HW1_Part3.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	int num, kind, i;
-	float probability = 1.0;
-	printf("1: base \t2: brocade \t3: \n");
-	printf("Please enter the number of card: ");
-	scanf("%d", &num);
-	printf("Please enter the kind of card: ");
-	scanf("%d", &kind);
-	for(i = 0; i < num; i++) {
-		if(kind == 1)
-			probability = probability*(30 - i)/(80 - i);
-		else if(kind == 2)
-			probability = probability*(34 - i)/(80 - i);
-		else if(kind == 3)
-			probability = probability*(16 - i)/(80 - i);
-		else
-			printf("Error: you enter wrong. \n");
-	}
-	printf("probability: %.10f \n", probability);
-	return 0;
+int main()
+{
+    int numCard, type, i;
+    float probability = 1.0;
+    char *card[3];
+    card[0] = "基本牌";
+    card[1] = "錦囊牌";
+    card[2] = "裝備牌";
+    printf("1: 基本牌\t 2: 錦囊牌\t 3: 裝備牌\n");
+    printf("請輸入牌型(1,2,3): ");
+    scanf("%d", &type);
+    printf("請輸入張數: ");
+    scanf("%d", &numCard);
+    for(i = 0; i < numCard; i++) {
+        if(type == 1){
+            if(numCard <= 34)
+                probability = probability*(34 - i)/(80 - i);
+            else {
+                printf("輸入張數: %d 超過黑色牌且是基本排張數: 34 \n", numCard);
+                probability = 0;
+                break;
+            }
+        }
+        else if(type == 2){
+            if(numCard <= 30)
+                probability = probability*(30 - i)/(80 - i);
+            else {
+                printf("輸入張數: %d 超過黑色牌且是錦囊牌張數: 30 \n", numCard);
+                probability = 0;
+                break;
+            }
+        }
+        else if(type == 3){
+            if(numCard <= 16)
+                probability = probability*(16 - i)/(80 - i);
+            else {
+                printf("輸入張數: %d 超過黑色牌且是裝備牌張數: 16 \n", numCard);
+                probability = 0;
+                break;
+            }
+        }
+        else {
+            printf("牌型輸入錯誤 \n");
+            probability = 0;
+            break;
+        }
+    }
+    if(probability == 0)
+        exit(EXIT_SUCCESS);
+    else
+        printf("選擇 %s 連續抽中 %d 張機率約為: %f", card[type-1], numCard, probability);
+    return 0;
 }
